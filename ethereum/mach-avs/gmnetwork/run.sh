79545c6fdce3cd5764c79be6e5457cb56a989218
fyInALT <97101459+fyInALT@users.noreply.github.com>
2025-04-25 17:42:42 +0800

    feat: upgrade operator tool version (#70)

--- ethereum/mach-avs/gmnetwork/run.sh
+++ ethereum/mach-avs/gmnetwork/run.sh
@@ -8,7 +8,7 @@ optOut() {
     --rm \
     --volume "${NODE_ECDSA_KEY_FILE_HOST}":/app/operator_keys/ecdsa_key.json \
     --volume "${NODE_BLS_KEY_FILE_HOST}":/app/operator_keys/bls_key.json \
-    public.ecr.aws/altlayer/mach-operator-tool:v0.2.5 \
+    public.ecr.aws/altlayer/mach-operator-tool:v0.3.2 \
     deregister-operator-with-avs
 }
 
